0cbedacf6d36f8a03eedc1c80612589940ab03ce
akanshabaishwade <akanshabaishwade@gmail.com>
2023-01-20 23:56:59 +0530

    [Add] all movies api

--- mainproject/app_1/api.py
+++ mainproject/app_1/api.py
@@ -1,14 +1,18 @@
+from urllib.request import Request
 from rest_framework import generics, permissions, mixins
 from rest_framework.response import Response
 from .serializer import RegisterSerializer, UserSerializer
 from django.contrib.auth.models import User
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
+from requests.auth import HTTPBasicAuth
 
 
-#Register API
+
+
+# Register API
 class RegisterApi(generics.GenericAPIView):
     serializer_class = RegisterSerializer
+
     def post(self, request, *args,  **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -19,4 +23,21 @@ class RegisterApi(generics.GenericAPIView):
         })
 
 
-  
+import requests
+import json
+from rest_framework.decorators import api_view
+
+
+@api_view(('GET',))
+def GetAllMovie(request):
+    response_API = requests.get('https://demo.credy.in/api/v1/maya/movies/')
+    all_data = response_API.text
+    print(response_API.text)
+    parse_json = json.loads(all_data)
+
+    data = {
+        'parse_json':parse_json,
+
+    }
+
+    return Response(data)
--- mainproject/app_1/urls.py
+++ mainproject/app_1/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('register/', RegisterApi.as_view()), # After register call (api/token/) to get token
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name ='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name ='token_refresh'),
+    path('movies/', GetAllMovie, name ='tokenGetAllMovie_refresh'),
+
 
 ]
